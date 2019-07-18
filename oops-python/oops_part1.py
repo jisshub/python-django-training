e5050a17ddd0ca2c07faf108d35a68f890d91a8f
jisshub <jissmon476@gmial.com>
2019-07-18 18:32:58 +0530

    oops started

--- oops-python/oops_part1.py
+++ oops-python/oops_part1.py
@@ -31,52 +31,61 @@
 #         amount = self.salary
 #         print(amount)
 
+#
+# class Player:
+#     def __init__(self, name, position, curr_skill_per):
+#         self.name = name
+#         self.position = position
+#         self.curr_skill_per = curr_skill_per
+#
+#     def status(self):
+#         matches_played = int(input('Enter no of matches: '))
+#         if matches_played > 10:
+#             self.curr_skill_per += (self.curr_skill_per * matches_played) / 100
+#             print(self.curr_skill_per)
+#         else:
+#             self.curr_skill_per -= (self.curr_skill_per * matches_played) / 100
+#             print(self.curr_skill_per)
+#
+#     def full_data(self):
+#         print(self.name, self.position, self.curr_skill_per)
+#
+#
+# player1 = Player('Hazard', 'LW', 70)
+#
+#
+# class Identity:
+#     dict = {}
+#
+#     def __init__(self, names, args):
+#         self.names = names
+#         self.id = args
+#
+#     def assign(self):
+#         for name in self.names:
+#             for id in self.id:
+#                 # update the dictionary
+#                 dict.update({name: id})
+#
+#         return dict
+#
+#
+# id1 = Identity(['jiss', 'jose'], [34, 54, 76])
+#
+# data = [3, 4, 5]
+# names = ['j', 'a', 'c']
+# for each in data:
+#     for name in names:
+#         print(f'{each}: {name}')
 
-class Player:
-    def __init__(self, name, position, curr_skill_per):
-        self.name = name
-        self.position = position
-        self.curr_skill_per = curr_skill_per
-
-    def status(self):
-        matches_played = int(input('Enter no of matches: '))
-        if matches_played > 10:
-            self.curr_skill_per += (self.curr_skill_per * matches_played) / 100
-            print(self.curr_skill_per)
-        else:
-            self.curr_skill_per -= (self.curr_skill_per * matches_played) / 100
-            print(self.curr_skill_per)
-
-    def full_data(self):
-        print(self.name, self.position, self.curr_skill_per)
 
 
-player1 = Player('Hazard', 'LW', 70)
 
 
-class Identity:
-    dict = {}
-
-    def __init__(self, names, args):
-        self.names = names
-        self.id = args
-
-    def assign(self):
-        for name in self.names:
-            for id in self.id:
-                # update the dictionary
-                dict.update({name: id})
-
-        return dict
 
 
-id1 = Identity(['jiss', 'jose'], [34, 54, 76])
 
-data = [3, 4, 5]
-names = ['j', 'a', 'c']
-for each in data:
-    for name in names:
-        print(f'{each}: {name}')
+
 
 
 
