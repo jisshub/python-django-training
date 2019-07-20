ca2c22c43bf67b4d59ce5847e03f7b5abd3e0a9a
jisshub <jissmon476@gmial.com>
2019-07-21 00:00:04 +0530

    decrorators in python

--- oops-python/decorators.py
+++ oops-python/decorators.py
@@ -61,7 +61,66 @@ class Employee:
 
 emp1 = Employee('jissmon', 'jose')
 
+
 # here v make an another instance methd called email and set the email in it.
 # and v place property decorator above the method.
 # and v can invoke this method as an attributes. ie..
 #     emp1.email
+
+
+# we can also add this property decrorator to full_name method and can invoke it as an attribute.
+
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return f"{self.first} {self.last}" + '@' + 'gmail.com'
+
+    @property
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+
+emp1 = Employee('Aju', 'asd')
+emp1.full_name = 'Jissmon Jose'
+
+
+# here v cannot set the values to the full name when it is invoked as an attribute.
+# ie. emp1.full_name='jidda asdas'
+# to set the values for the attribute, v use setter.
+
+
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return f"{self.first} {self.last}" + '@' + 'gmail.com'
+
+    @property
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    @full_name.setter
+    def full_name(self, name):
+        self.first = name.split()[0]
+        self.last = name.split()[1]
+        return f"{self.first} {self.last}"
+
+    @email.setter
+    def email(self, email_id):
+        return email_id
+
+
+emp1 = Employee('Aju', 'asd')
+
+# here setter for the full_name method is, @full_name.setter
+# now v can set the value for the full_name while it is invoked.
+# here, name parameter recerves the value v set for the full_name.
+# eg:emp1.full_name='jissmon jose'
+# here value 'jissmon jose' is passed to the name parameter.
