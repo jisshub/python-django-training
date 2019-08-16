ad1f3ab4f40bdca0022d8e9ead91650e34dbb7c8
jisshub <jissmon476@gmial.com>
2019-08-17 00:20:04 +0530

    started __name__ special variable

---
+++ first.py
@@ -0,0 +1,9 @@
+
+print(__name__)
+
+
+# when a python runs a file direclty, bfore running any code ,
+# it sets a special variable __name__ equal to __main__.
+# so, when v print this, it gives __main__ as the result
+
+
